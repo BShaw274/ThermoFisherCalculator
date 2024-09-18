d48e2828939d3071ce5674498ca18eb364f2cd19
BShaw274 <bts9756@gmail.com>
2024-09-18 14:45:59 -0600

    first commit

---
+++ TFCalculator/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TFCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        CalculatorObject cal = new CalculatorObject();
+
+        //All button controls
+        private void Button_Click_Addition(object sender, RoutedEventArgs e)
+        {
+            cal.setNums(double.Parse(Message1.Text));
+            cal.setOperand(Operation.addition);
+            Message1.Text = "";
+        }
+
+        private void Button_Click_Subtraction(object sender, RoutedEventArgs e)
+        {
+            cal.setNums(double.Parse(Message1.Text));
+            cal.setOperand(Operation.subtraction);
+            Message1.Text = "";
+        }
+
+        private void Button_Click_Multiplication(object sender, RoutedEventArgs e)
+        {
+            cal.setNums(double.Parse(Message1.Text));
+            cal.setOperand(Operation.multiplication);
+            Message1.Text = "";
+        }
+
+        private void Button_Click_Division(object sender, RoutedEventArgs e)
+        {
+            cal.setNums(double.Parse(Message1.Text));
+            cal.setOperand(Operation.division);
+            Message1.Text = "";
+        }
+        private void Button_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            cal.nums.Clear();
+            Message1.Text = "";
+        }
+        private void Button_Click_Equals(object sender, RoutedEventArgs e)
+        {
+            cal.setNums(double.Parse(Message1.Text));
+            Message1.Text = cal.results();
+        }
+        private void Button_Click_Flip(object sender, RoutedEventArgs e)
+        {
+            double num = double.Parse(Message1.Text);
+            num = num * (-1);
+            Message1.Text = num.ToString();
+        }
+        //Number buttons
+
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "0";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "1";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "2";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "3";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "4";
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "5";
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "6";
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "7";
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "8";
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += "9";
+        }
+
+        private void Button_Click_Dot(object sender, RoutedEventArgs e)
+        {
+            Message1.Text += ".";
+        }
+    }
+}
---
+++ TFCalculator/calculatorObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TFCalculator
+{
+    public enum Operation
+    {
+        addition = 0,
+        subtraction = 1,
+        multiplication = 2,
+        division = 3
+    }
+
+    public class CalculatorObject
+    {
+        
+        public CalculatorObject() 
+        {
+            result = 0;
+            nums = new List<double>();
+        }
+        public double result;
+        public List<double> nums;
+        Operation operand;
+
+        public void setNums(double num)
+        {
+
+            nums.Add(num);
+        }
+
+        public void setOperand(Operation inOp)
+        {
+            operand = inOp;
+
+        }
+        //calculates the results using the internal operation enum
+        public string results()
+        {
+            double results = 0;
+            switch (operand)
+            {
+                case Operation.addition:
+                    results = nums[0] + nums[1];
+                    break;
+                case Operation.subtraction:
+                    results = nums[0] - nums[1];
+                    break;
+                case Operation.multiplication:
+                    results = nums[0] * nums[1];
+                    break;
+                case Operation.division:
+                    results = nums[0] / nums[1];
+                    break;
+            }
+            //clears the numbers saved internally
+            nums.Clear();
+            return results.ToString();
+
+        }
+    }
+
+}
